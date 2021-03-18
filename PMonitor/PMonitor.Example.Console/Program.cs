308db6546b1395926c941365b147b4795418a3b4
stephan <stephan_rothe79@outlook.com>
2021-03-18 15:09:49 +0300

    monitoring time changed

--- PMonitor/PMonitor.Example.Console/Program.cs
+++ PMonitor/PMonitor.Example.Console/Program.cs
@@ -10,6 +10,7 @@ namespace PMonitor.BoehmTrader.Console
     class Program
     {
         private static string processLocation = @"C:\Users\tpaul\source\repos\BoehmTester\BoehmTrader\bin\Debug\BoehmTrader.exe";
+        private static long numMonitoring = 0;
         /// <summary>
         /// On Windows, interrogate the status of the Notepad process every 3 seconds. You can open and close
         /// Notepad in order to see how the program responds to your changes. We print the output on the console.
@@ -21,20 +22,22 @@ namespace PMonitor.BoehmTrader.Console
             while (true)
             {
                 pm.RefreshInformation();
-                BasicProcessInformation bpi = pm.GetProcessInformation().Single();                
+                BasicProcessInformation bpi = pm.GetProcessInformation().Single();
 
-                System.Console.WriteLine("{0} Process {1} is {2}", DateTime.Now.ToString(CultureInfo.InvariantCulture), bpi.FriendlyName, bpi.State.ToString());
+                if (numMonitoring % 10 == 0)
+                    System.Console.WriteLine("{0} Process {1} is {2}", DateTime.Now.ToString(CultureInfo.InvariantCulture), bpi.FriendlyName, bpi.State.ToString());
 
                 if (bpi.State == ProcessState.NotRunning)
                     StartProcess();
 
-                Thread.Sleep(3000);
+                Thread.Sleep(500);
+                numMonitoring++;
             }
         }
 
         private static void StartProcess()
         {
-            Thread.Sleep(3000);
+            Thread.Sleep(500);
             System.Console.WriteLine("BoehmTrader will restart--");
             Process.Start(processLocation);
         }
